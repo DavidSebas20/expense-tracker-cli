e1fcc328cb8587ca848dfeeaa779d65fab5a9220
DavidSebas20 <davidsebastian20@outlook.com>
2024-08-24 00:02:18 -0500

    Agregada la funcion para mostrar la lista de los gastos agregados

--- expense_tracker.py
+++ expense_tracker.py
@@ -20,7 +20,8 @@ def cargar_archivo():
 def gruardar_gastos(tasks):
     with open(ARCHIVO_GASTOS, 'w') as file:
         json.dump(tasks, file, indent=2)
-    
+
+#Funcion para añadir un gasto    
 def add_gasto(description, mount):
     gastos = cargar_archivo()
     new_id = max(gasto['id'] for gasto in gastos) + 1 if gastos else 1
@@ -34,6 +35,14 @@ def add_gasto(description, mount):
     gruardar_gastos(gastos)
     print(f"Gasto añadido correctamente (ID: {new_id})")
 
+#Funcion para mostrar la lista de los gastos
+def mostrar_gastos():
+    gastos = cargar_archivo()
+    print("ID\t Fecha\t\t Descripcion\t Monto")
+    for gasto in gastos:
+        print(gasto['id'],"\t",gasto['creacion'],"\t",gasto['descripcion'],"\t$",gasto['monto'])
+
+
 
 parser = argparse.ArgumentParser(description="Seguimiento de Gastos CLI")
     
@@ -43,9 +52,16 @@ subparsers = parser.add_subparsers(dest="command")
 add_parser = subparsers.add_parser("add", help="Añadir un nuevo gasto")
 add_parser.add_argument("--descripcion", required=True, help="Descripcion del gasto")    
 add_parser.add_argument("--monto", type=float, required=True, help="Monto del gasto")
+
+
+# Comando para agregar un gasto
+add_parser = subparsers.add_parser("list", help="Mostrar todos los gastos de la lista")
+
 args = parser.parse_args()
 
 if args.command == "add":
     add_gasto(args.descripcion, args.monto)
+elif args.command == "list":
+    mostrar_gastos()
 else:
     parser.print_help()
